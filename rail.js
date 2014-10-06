949eb7df60a0df6fb0fe8667a26dfbe3595de52d
Gregory Shtrasberg <shtras@gmail.com>
2014-10-06 22:23:00 +0200

    Redesign

--- rail.js
+++ rail.js
@@ -136,9 +136,9 @@ var values = [
     'service',
 ];
 
+var trainCount=0;
+
 function getTime(label) {
-    var dbg1 = $("#wait_time").val();
-    var dbg2 = $(label).val();
     var parts = $(label).val().split(':');
     return parseInt(parts[0])*60+parseInt(parts[1]);
 }
@@ -154,6 +154,15 @@ function getRefValue(value) {
 
 function getDesValue(value) {
     return $("#des_"+value).val();
+}
+
+function calcTrain(train, length, hours) {
+    var condition = 100 - (100 - train.reliability)*hours/24;
+    var averageCond = (100-condition)/2+condition;
+    var accTime = train.speed/train.acc;
+    var speed = train.speed*averageCond/100;
+    var resTime = (length+speed*accTime-train.acc*accTime*accTime/2)/speed*2;
+    return {time: resTime, cond: condition};
 }
 
 function recalc() {
@@ -181,56 +190,148 @@ function recalc() {
                    'Condition in the end: '+desCond.toFixed(2)+'<br/>');
 }
 
+function updateLength(){
+    var wTime = getTime("#wait_time");
+    var rtt = getTime("#rtt");
+    var oneWayTime = (rtt-wTime)/2;
+    var speed = $("#speed_base").val();
+    var acc = $("#acc_base").val();
+    var hours = parseInt($("#hours").val());
+    var accTime = speed/acc;
+    var length = acc*accTime*accTime/2+speed*(oneWayTime-accTime);
+    $("#length").val(length);
+}
+
+function updateResult(id){
+    var train = {
+        speed: parseInt($("#speed_"+id).val()),
+        acc: parseInt($("#acc_"+id).val()),
+        reliability: parseInt($("#reliability_"+id).val()),
+        wagons: parseInt($("#wagons_"+id).val()),
+    };
+    var length = parseFloat($("#length").val());
+    var hours = $("#hours").val();
+    var res = calcTrain(train, length, hours);
+    var time = res.time;
+    var cond = res.cond;
+    time += getTime('#wait_time');
+    var perHour = parseInt($("#price").val())*(3600/time)*train.wagons;
+    $("#income_"+id).val(perHour.toFixed(2));
+    $("#net_income_"+id).val((perHour*hours).toFixed(2));
+    $("#condition_"+id).val(cond.toFixed(2));
+    $("#trip_time_"+id).val(formatTime(time));
+}
+
+function removeTrain(id){
+    $("#block_"+id).remove();
+}
+
+function addResultBlock(id){
+    var block = $("<div/>")
+        .append($("<div/>").attr("class", "left")
+            .append(document.createTextNode('Avg income')))
+        .append($("<div/>").attr("class", "right")
+            .append($("<input/>").attr('id', 'income_'+id)))
+        .append($("<br/>"))
+        .append($("<div/>").attr("class", "left")
+            .append(document.createTextNode('Net Income')))
+        .append($("<div/>").attr("class", "right")
+            .append($("<input/>").attr('id', 'net_income_'+id)))
+        .append($("<br/>"))
+        .append($("<div/>").attr("class", "left")
+            .append(document.createTextNode('Avg trip time')))
+        .append($("<div/>").attr("class", "right")
+            .append($("<input/>").attr('id', 'trip_time_'+id)))
+        .append($("<br/>"))
+        .append($("<div/>").attr("class", "left")
+            .append(document.createTextNode('Condition')))
+        .append($("<div/>").attr("class", "right")
+            .append($("<input/>").attr('id', 'condition_'+id)));
+           
+    return block;
+}
+
+function addTrain() {
+    var myCount = trainCount;
+    var select = $("<select/>").attr("id", 'train_'+myCount).change(function(){updateValues(myCount);updateResult(myCount);});
+    for (var i in trains) {
+        var train = trains[i];
+        select.append($("<option></option>").attr("value", i).text(train.name));
+    }
+    var trainBlock = $("<div/>").attr("class", "train_details").attr("id", "block_"+myCount).append(select)
+        .append($("<br/>"));
+    
+    for (var i in values) {
+        var value = values[i];
+        trainBlock
+        .append($("<div/>").attr("class", "left")
+            .append(document.createTextNode(value+': '))
+        ).append($("<div/>").attr("class", "right")
+            .append($("<input/>").attr("id", value+'_'+myCount).change(function(){updateResult(myCount)}))
+        ).append($("<br/>"));
+    }
+    trainBlock.append($("<input/>").attr('type', 'submit').val('Remove').click(function(){removeTrain(myCount);}));
+    trainBlock.append(addResultBlock(myCount));
+    $("#trains_select").append(trainBlock);
+    updateValues(myCount);
+    ++trainCount;
+}
+
+function updateAll() {
+    updateLength();
+    for (var i=0; i<trainCount; ++i) {
+        updateResult(i);
+    }
+}
+
 function initAll() {
     for (var i in trains) {
         var train = trains[i];
-        $("#ref_train").append($("<option></option>").attr("value", i).text(train.name));
-        $("#des_train").append($("<option></option>").attr("value", i).text(train.name));
+        $("#train_base").append($("<option></option>").attr("value", i).text(train.name));
         train.service = Math.round(train.price*0.17);
     }
     
     for (var i in values) {
         var value = values[i];
         $("#reference")
-        .append(document.createTextNode(value+': '))
-        .append($("<input/>").attr("id", "ref_"+value).change(function(){recalc()}));
+        .append($("<div/>").attr("class", "left")
+            .append(document.createTextNode(value+': '))
+        ).append($("<div/>").attr("class", "right")
+            .append($("<input/>").attr("id", value+"_base").change(function(){udateAll()}))
+        ).append($("<br/>"));
     }
     
     for (var i in values) {
         var value = values[i];
         $("#desired")
         .append(document.createTextNode(value+': '))
-        .append($("<input/>").attr("id", "des_"+value).change(function(){recalc()}));
+        .append($("<input/>").attr("id", "des_"+value).change(function(){udateAll()}));
     }
-    updateValues('ref');
-    updateValues('des');
+    updateValues('base');
 }
 
-function updateValues(prefix) {
-    var train = trains[$("#"+prefix+"_train option:selected").val()];
+function updateValues(suffix) {
+    var train = trains[$("#train_"+suffix+" option:selected").val()];
     if (!train) {
         return;
     }
     
     for (var i in values) {
         var value = values[i];
-        $("#"+prefix+'_'+value).val(train[value]);
+        $('#'+value+'_'+suffix).val(train[value]);
     }
 }
 
 function registerCallbacks() {
-    $("#ref_train").change(function() {
-        updateValues("ref");
-        recalc();
+    $("#train_base").change(function() {
+        updateValues("base");
+        updateAll();
     });
-    $("#des_train").change(function() {
-        updateValues("des");
-        recalc();
-    });
-    $("#wait_time").change(function(){recalc();});
-    $("#rtt").change(function(){recalc();});
-    $("#price").change(function(){recalc();});
-    $("#recalc").click(function(){recalc();});
+    $("#wait_time").change(function(){updateAll();});
+    $("#rtt").change(function(){updateAll();});
+    $("#price").change(function(){updateAll();});
+    $("#recalc").click(function(){updateAll();});
+    $("#add_train").click(function(){addTrain();});
 }
 
 $(document).ready(function() {
